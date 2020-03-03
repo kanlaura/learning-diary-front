88932d21ce006c7d34e1541bd8a3008b8bd9b271
kanlaura <59911290+kanlaura@users.noreply.github.com>
2020-03-03 07:54:20 +0200

    delete button added

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Post extends Component {
+    editRow = () => {
+        console.log('edit this ' + this.props.post.id)
+    }
+    deleteRow = () => {
+        this.props.deletePost(this.props.post.id)
+    }
+
     render() {
-        const {id, title, description, timetomaster, timespent, source, startlearningdate, inprogress} = this.props.post
+        const { id, title, description, timetomaster, timespent, source, startlearningdate, inprogress } = this.props.post
         return (
             <div>
-                <p>{id} {title} {description} {timetomaster} {timespent} {source} {startlearningdate} {inprogress}</p>
+                <p>{id} {title} {description} {timetomaster} {timespent} {source} {startlearningdate} {inprogress} <button onClick={this.deleteRow}>X</button><button onClick={this.editRow}>edit</button></p>
             </div>
         )
     }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -14,12 +14,18 @@ export default function Posts() {
         fetchData()
     }, []);
 
+    const deletePost = async (id) => {
+        console.log('delete post ' + id)
+        await axios.delete(`/api/${id}`)
+        fetchData()
+    }
+
     return (
         <Fragment>
             <h2>Posts</h2>
             <div>
                 {post.posts.map((post, i) => (
-                    < Post post={post} key={post.id} />
+                    < Post post={post} key={post.id} deletePost={deletePost}/>
                 ))}
             </div>
         </Fragment>
